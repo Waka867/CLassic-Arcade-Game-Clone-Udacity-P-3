b68ff7fb1cb233c5b19db555c459ac69886e0b35
Waka867 <ledwing.hernandez@gmail.com>
2015-10-21 16:24:52 -0400

    more struggling - got background to render though
    - went through error console trying to squash errors until finally the
    bkg rendered
    - added player.prototype.update much like enemy
    - added Player class
    - added x and y params to Enemy
    - added this.x and this.y to Enemy
    - added correct this.sprite to Player class
    - added handleInput and render to Player class
    - added allEnemies array and set up for loop that adds 4 Enemy() calls
    that are each pushed to array
    - created player var as new Player classed object
    - commented out html ctx.rect and ctx.stroke as well as new img and
    img.addevenlistener to load. Also commented out the img.src

--- js/app.js
+++ js/app.js
@@ -1,13 +1,38 @@
 // Enemies our player must avoid
-var Enemy = function(loc) {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.loc = loc;
+
+    this.x = x;
+    this.y = y;
+    // this.speed = speed;
+    // var obj = {};
+    // return obj;
+
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
+
+
+
+var move = function() {
+    this.loc++;
+};
+
+
+
+// var amy = Enemy(1);
+// amy.move();
+// var ben = Enemy(9);
+// ben.move();
+// var pam = Enemy(4);
+// amy.move();
+// var dan = Enemy(2);
+// ben.move();
+
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -22,23 +47,36 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+// Now write your own Player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function(){
+var Player = function(x, y){
+    var x = x;
+    var y = y;
+    Player.handleInput = function(){
+    };
 
     this.sprite = 'images/char-boy.png';
-    this.update();
-    this.handleInput();
-    this.render();
+    Player.render = function(){
+    };
 };
+
+Player.prototype.update = function(dt){
+
+};
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-// var allEnemies = new array[Enemy()];
-// var player = Player();
+var allEnemies = [];
+for(var i = 0; i <= 4; i++) {
+    pushEnemy = new Enemy();
+};
+
+var player = new Player();
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -52,6 +90,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-
-
