a591a08c80fb7b664ea00adb25f39117c60883ed
Waka867 <ledwing.hernandez@gmail.com>
2015-10-23 13:09:02 -0400

    got enemies to display as well as move
    - added placeholder h1 points tag
    - called four new instances of Enemy and pushed those to allEnemies
    array
    - tried Enemy reset logic with no luck
    - tried player reset logic with no luck (once I get one the other should
    be easy)

--- js/app.js
+++ js/app.js
@@ -8,20 +8,18 @@ var Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
 };
 
-Enemy.prototype.move = function(x, y) {
-    this.x++;
-};
-
-
-var amy = new Enemy(200, 320, 200);
-amy.move();
-// var ben = Enemy(250, 320, 200);
-// ben.move();
-// var pam = Enemy(300, 320, 200);
+var amy = new Enemy(-90, 320, 200);
 // amy.move();
-// var dan = Enemy(100, 320, 200);
+var ben = new Enemy(-100, 200, 100);
+// ben.move();
+var pam = new Enemy(-100, 120, 500);
+// amy.move();
+var dan = new Enemy(-120, 70, 300);
 // ben.move();
 
+Enemy.prototype.move = function(x, y, speed) {
+    this.x += this.x++;
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -29,11 +27,20 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Enemy.prototype.reset = function(){
+    if(this.x >= 570){
+        this.x = -90;
+        this.y = y;
+        this.speed = Math.random()*1000;
+    };
 };
 
 // Now write your own Player class
@@ -44,7 +51,17 @@ var Player = function(x, y){
     this.x = x;
     this.y = y;
     this.sprite = 'images/char-boy.png';
+    if(this.y < 50) {
+        this.x = 200;
+        this.y = 420;
+    }else{};
 };
+
+// var playerReset = function(player){
+//     player.x = 200;
+//     player.y = 420;
+// };
+
 
 Player.prototype.update = function(dt){
 };
@@ -72,14 +89,20 @@ Player.prototype.handleInput = function(direction){
     // };
 };
 
+Player.prototype.reset = function(x, y){
+    if(this.y < 50) {
+        this.x = 200;
+        this.y = 420;
+    };
+};
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+
 var allEnemies = [];
-for(var i = 0; i <= 4; i++) {
-    pushEnemy = new Enemy();
-};
+allEnemies.push(amy, ben, pam, dan);
 
 var player = new Player(200, 420);
 
